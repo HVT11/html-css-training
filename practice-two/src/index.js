656800b0e04cc6c1994ea707d6e7ea6b027bbca4
HVT11 <huynhtri11102000@gmail.com>
2022-05-19 09:53:08 +0700

    Fix ui and add move top button

--- practice-two/src/index.js
+++ practice-two/src/index.js
@@ -9,3 +9,18 @@ function closeMenu() {
     document.getElementById('btn-open-menu').classList.remove('header-top__toggle-open--disable')
     document.getElementById('btn-close-menu').classList.remove('header-top__toggle-close--active')
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
